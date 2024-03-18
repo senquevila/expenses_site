937e03651198ea7bf5fa7cc02e17053066f8c831
Enrique Avila <senquevila@gmail.com>
2024-03-18 00:59:58 -0600

    rename models in admin

--- expenses/management/commands/populate_local_amount.py
+++ expenses/management/commands/populate_local_amount.py
@@ -1,12 +1,12 @@
 from django.core.management.base import BaseCommand
-from expenses.models import Expense
+from expenses.models import Transaction
 
 
 class Command(BaseCommand):
     help = "Populate the expenses' local amounts"
 
     def handle(self, *args, **options):
-        for expense in Expense.objects.exclude(local_amount=0, currency__alpha3="HND"):
+        for expense in Transaction.objects.exclude(local_amount=0, currency__alpha3="HND"):
             expense.local_amount = expense.get_local_amount
             expense.save()
             print(f"Updated expense {expense.id}")
--- expenses/management/commands/refresh_period_total.py
+++ expenses/management/commands/refresh_period_total.py
@@ -1,14 +1,14 @@
 from django.core.management.base import BaseCommand
 from django.db.models import Sum
 
-from expenses.models import Expense, Period
+from expenses.models import Transaction, Period
 
 
 class Command(BaseCommand):
     help = "Recalculate the period total"
 
     def handle(self, *args, **options):
-        for expense in Expense.objects.values("period").annotate(total=Sum("local_amount")):
+        for expense in Transaction.objects.values("period").annotate(total=Sum("local_amount")):
             period = Period.objects.get(pk=expense["period"])
             period.total = expense["total"]
             period.save()
